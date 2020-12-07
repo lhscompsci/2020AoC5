f52d7a13aa5ac0190f6c051a4d150d82570653a7
lhscompsci <sam.black@lubbockisd.org>
2020-12-07 16:13:01 -0600

    Not quite working yet

--- src/com/wtxcompsci/Main.java
+++ src/com/wtxcompsci/Main.java
@@ -3,12 +3,16 @@ package com.wtxcompsci;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.util.Arrays.asList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner inf = new Scanner(new File(("input.dat")));
+        Scanner inf = new Scanner(new File(("inputbasic.dat")));
 
         int largest = -1;
 
@@ -19,7 +23,9 @@ public class Main {
         }
 
         for(String item: list){
-            int val = getSeatID(list);
+            System.out.println(item);
+            int val = getSeatID(item);
+            System.out.println(val);
             if(val>largest){
                 largest = val;
             }
@@ -30,9 +36,51 @@ public class Main {
     }
 
     public static int getSeatID(String pass){
-        int seatID;
+        int seatID=8;
 
+        boolean[] rows = new boolean[128];
+        boolean[] seats = new boolean[8];
 
-        return seatID;
+        Arrays.fill(rows,true);
+        Arrays.fill(seats,true);
+
+        System.out.println(pass);
+
+        String[] steps = pass.split("");
+        int start = 0, end = 127;
+        for(int i = 0; i<7; i++){
+            System.out.println(i+": "+steps[i]);
+            if(steps[i].equals("F")){
+                end = (start + end) / 2 + 1;
+            }
+            if(steps[i].equals("B")){
+                start = end/2;
+            }
+            flipList(rows,start,end);
+
+        }
+        start = 0;
+        end = 7;
+        System.out.println(Arrays.toString(seats));
+        for(int i= 7; i<10; i++){
+            System.out.println(i+": "+steps[i]);
+            if(steps[i].equals("R")){
+                end = (start + end+1) / 2 ;
+            }
+            if(steps[i].equals("L")){
+                start = end/2+1;
+            }
+            flipList(seats,start,end);
+            System.out.println(Arrays.toString(seats));
+        }
+
+        seatID = seatID * asList(rows).indexOf(true) + asList(seats).indexOf(true);
+
+        return seatID ;
+    }
+
+    public static void flipList(boolean[] l, int s, int f){
+        for(int i=s; i<f; i++)
+            l[i]=false;
     }
 }
